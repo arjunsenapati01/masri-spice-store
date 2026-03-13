12f4696e329affeff745af1aa9e5c8c5967b2dd0
Arjun <arjun@Mac.lan>
2026-03-13 18:31:19 +0530

    fix: restore ES module syntax for test API

--- api/test.js
+++ api/test.js
@@ -1,7 +1,7 @@
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
     res.status(200).json({
-        message: 'CommonJS API results',
+        message: 'API is working!',
         node_version: process.version,
         env_keys: Object.keys(process.env).filter(k => k.includes('POSTGRES') || k.includes('ADMIN') || k.includes('BLOB'))
     });
-};
+}
